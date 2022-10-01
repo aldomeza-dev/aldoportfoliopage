8623f82698e1862b4c28eb1eb87ad6cf43429f40
Aldo_Meza <aldomezamoreno@gmail.com>
2022-10-01 00:16:00 -0500

    Setting up tallwind and main component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css'
 function App() {
   return (
     <div className='App'>
-      
+      <div className='flex justify-center'>
+        <h1 className='mainTittle leading-normal text-6xl text-transparent bg-clip-text bg-gradient-to-r from-medium-slate-purple to-munsell-blue '>
+          Welcome, my name is Aldo Meza...
+        </h1>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/App.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'space-cadet-black': '#2D3142',
+        'munsell-blue': '#1b9aaa',
+        'majorelle-purple': '#5647de',
+        'medium-slate-purple': '#7568f0',
+        'alabaster-white': 'e3e4db',
+      },
+    },
   },
   plugins: [],
 }
