2f4a7106080fd036e3092305705be58b23d95d00
BecarioDeTiOriginalGroup <becario.ti@original-group.com>
2022-10-02 20:08:12 -0500

    Animate.CSS and Typewriter.js instalation and maintittle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css'
+import 'animate.css'
+import { Typewriter } from 'react-simple-typewriter'
 
 function App() {
   return (
     <div className='App'>
-      <div className='flex justify-center'>
-        <h1 className='mainTittle leading-normal text-6xl text-transparent bg-clip-text bg-gradient-to-r from-medium-slate-purple to-munsell-blue '>
-          Welcome, my name is Aldo Meza...
+      <div className='flex flex-col items-center'>
+        <h1 className='mainTittle1 mainTittleFont animate__animated animate__fadeInDown animate__delay-1s mt-10 lg:mt-16 text-2xl lg:text-7xl font-light lg:leading-normal text-transparent bg-clip-text bg-gradient-to-r from-medium-slate-purple to-munsell-blue'>
+          Welcome, I'm Aldo Meza...
+        </h1>
+        <h1 className='mainTittle2 mainTittleFont animate__animated animate__fadeInDown animate__delay-1s text-2xl lg:text-7xl font-light lg:leading-normal text-transparent bg-clip-text bg-gradient-to-r from-medium-slate-purple to-munsell-blue'>
+          and I'm a&nbsp;
+          <span>
+            <Typewriter
+              loop
+              cursor
+              delaySpeed={2500}
+              words={['web', 'embeded', 'mobile']}
+            />
+          </span>
+          &nbsp;developer!
         </h1>
       </div>
     </div>
